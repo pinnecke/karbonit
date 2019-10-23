982e8023e0cfd608c2189861da0c26de5cc0b1e2
Marcus Pinnecke <marcus.pinnecke@gmail.com>
2019-10-23 11:38:45 +0200

    Fix name issue after refactor

--- examples/carbon-api/browse-contents/read-record-contents.c
+++ examples/carbon-api/browse-contents/read-record-contents.c
@@ -7,7 +7,7 @@ int main (void)
 {
         carbon record;
         err err;
-        carbon_array_it it;
+        struct carbon_array it;
         //string_buffer str;
 
         carbon_from_json(&record, "[\"Hello\", \"Number\", 23]", CARBON_KEY_NOKEY, NULL, &err);

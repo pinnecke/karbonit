c6d494a3a1c0476ac77d40a3e04e09974e8cf304
Marcus Pinnecke <marcus.pinnecke@gmail.com>
2019-10-28 18:21:56 +0100

    no message

--- bench/json/fast-parse/main.cpp
+++ bench/json/fast-parse/main.cpp
@@ -2,10 +2,9 @@
 #include <fcntl.h>
 #include <jakson/jakson.h>
 
-int main(void) {
-
-        /* the working directory must be 'tests/jakson-tool' to find this file */
-        int fd = open("bench/json/fast-parse/assets/random.json", O_RDONLY);
+void run(const char *path, int sample)
+{
+        int fd = open(path, O_RDONLY);
         int json_in_len = lseek(fd, 0, SEEK_END);
         const char *json_in = (const char *) mmap(0, json_in_len, PROT_READ, MAP_PRIVATE, fd, 0);
 
@@ -13,11 +12,33 @@ int main(void) {
         json json;
         json_err error_desc;
         json_parser_create(&parser);
-        bool status = json_parse(&json, &error_desc, &parser, json_in);
 
-        std::cout << (status ? "Jupp" : "Nope") << std::endl;
+        timestamp start = wallclock();
+        json_parse(&json, &error_desc, &parser, json_in);
+
+        timestamp end = wallclock();
+
+        std::cout << path << ";" << sample << ";" << (end - start) << std::endl;
+
 
         json_drop(&json);
+       // close(fd);
+}
+
+void f(const char *g) {
+        for (unsigned i = 0; i < 15; i++) {
+                run(g, i);
+        }
+}
+
+int main(void) {
+
+        std::cout << "file;sample;read-time-ms"<< std::endl;
+        f("bench/json/fast-parse/assets/mag-1.json");
+        f("bench/json/fast-parse/assets/mag-2.json");
+        f("bench/json/fast-parse/assets/mag-3.json");
+        f("bench/json/fast-parse/assets/mag-4.json");
+
 
         return 0;
 }

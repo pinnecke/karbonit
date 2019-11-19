4e8a58c91a0868974df65f679bca6f0b641598fb
Marcus Pinnecke <marcus.pinnecke@gmail.com>
2019-11-19 15:06:32 +0100

    refactor

--- src/jakson/carbon/array.h
+++ src/jakson/carbon/array.h
@@ -1,23 +1,11 @@
-/**
- * Columnar Binary JSON -- Copyright 2019 Marcus Pinnecke
- * This file implements an (read-/write) iterator for (JSON) arrays in carbon
+/*
+ * arr_it - array iterator implementation
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
- * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
- * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
- * the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
- * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
- * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * Copyright 2019 Marcus Pinnecke
  */
 
-#ifndef CARBON_ARRAY_H
-#define CARBON_ARRAY_H
+#ifndef HAD_ARR_IT_H
+#define HAD_ARR_IT_H
 
 // ---------------------------------------------------------------------------------------------------------------------
 //  includes
